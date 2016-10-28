a49fa6acb194ee845b025f64249aa9a8b539d118
Alexandre <aestebanez@gmail.com>
2016-10-28 13:20:59 -0200

    subindo o projeto

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function gotoExcursoes() {
+    alert('here');
+    $('#solTitle a').click(function(evt) {
+        evt.preventDefault();
+        alert('here in');
+        var divId = 'summary' + $(this).attr('id');
+
+        document.getElementById(divId).className = '';
+
+    });
+}
+
+$('#sticky').stickable();
